0f68ce055f9fc5dcaf5ff99540e64a9f7340ba52
Paul <paul_1mk@hotmail.com>
2015-05-06 21:34:40 -0600

    *Ya estan las tablas empleados y sucrursales no importa el nombre de la BD noas importenla y pongan packets

---
+++ 127_0_0_1.sql
@@ -0,0 +1,139 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 07-05-2015 a las 05:32:27
+-- Versión del servidor: 5.6.21
+-- Versión de PHP: 5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `packets`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clientes`
+--
+
+CREATE TABLE IF NOT EXISTS `clientes` (
+`ID` int(5) NOT NULL,
+  `nombre` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `apellido_paterno` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
+  `apellido_materno` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
+  `nombre_compañia` varchar(70) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `RFC` varchar(12) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `telefono` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
+  `direccion` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `numero` int(6) NOT NULL,
+  `CP` int(5) NOT NULL,
+  `ciudad` varchar(25) COLLATE utf8_spanish_ci NOT NULL,
+  `estado` varchar(25) COLLATE utf8_spanish_ci NOT NULL,
+  `pais` varchar(25) COLLATE utf8_spanish_ci NOT NULL,
+  `puesto` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
+  `titulo` varchar(10) COLLATE utf8_spanish_ci NOT NULL,
+  `email` varchar(40) COLLATE utf8_spanish_ci NOT NULL,
+  `nombre_usuario` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
+  `contraseña` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `fecha_registro` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `fecha_eliminado` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleados`
+--
+
+CREATE TABLE IF NOT EXISTS `empleados` (
+`id` int(15) NOT NULL,
+  `nombre` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `a_paterno` varchar(40) COLLATE utf8_spanish_ci NOT NULL,
+  `a_materno` varchar(40) COLLATE utf8_spanish_ci NOT NULL,
+  `telefono` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
+  `direccion` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `calle` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `numero` varchar(15) COLLATE utf8_spanish_ci NOT NULL,
+  `cp` int(15) NOT NULL,
+  `sucursal` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `correo` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `tipo_empleado` int(11) NOT NULL,
+  `fecha_registro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `fecha_eliminado` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sucursales`
+--
+
+CREATE TABLE IF NOT EXISTS `sucursales` (
+`id` int(11) NOT NULL,
+  `nombre_sucursal` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `direccion` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `calle` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `numero` int(15) NOT NULL,
+  `cp` varchar(10) COLLATE utf8_spanish_ci NOT NULL,
+  `ciudad` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `estado` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `pais` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `telefono` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `fecha_registro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `fecha_eliminado` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+ ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `empleados`
+--
+ALTER TABLE `empleados`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `sucursales`
+--
+ALTER TABLE `sucursales`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+MODIFY `ID` int(5) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `empleados`
+--
+ALTER TABLE `empleados`
+MODIFY `id` int(15) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `sucursales`
+--
+ALTER TABLE `sucursales`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
