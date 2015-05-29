aee41846bc13f079fbc8a1eb58dcc68ee9864304
Lugo Aguilar Humberto <lugoe_@hotmail.com>
2015-05-29 03:32:34 -0600

    *Agregre el proyecto ANDROID!

--- Web Services/packetsWS/queryDataCustomer.php
+++ Web Services/packetsWS/queryDataCustomer.php
@@ -1,4 +1,5 @@
 
+
 <?php
 
 //$array = json_decode(entity, true);
@@ -23,7 +24,7 @@ mysql_select_db('packets') or die('No se pudo seleccionar la base de datos');
 
 
 // Realizar una consulta MySQL
-$query = "SELECT username, password FROM customers WHERE username = '".$value['user']."'";
+$query = "SELECT name, last_name, phone, address, postal_code, city, state, country, degree, email, username, company_name, rfc, position, registration_date FROM customers WHERE username = '".$value['user']."'";
 $result = mysql_query($query) or die('Consulta fallida: ' . mysql_error());
 $fila = mysql_fetch_row($result);
 // Imprimir los resultados en HTML
@@ -38,25 +39,15 @@ while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
 }
 //echo "</table>\n";
 
-//echo $fila[0];
-//echo ' == ';
-//echo $value['user'];
-   
-        
-if($fila[0] == $value['user']){
+   		//echo $fila[11];
+   		//echo $fila[12];
+   		//echo $fila[13];
+        //echo $fila[14];
     
-//echo $fila[1];
-//echo ' == ';
-//echo MD5($value['password']);
 
-	if($fila[1] == MD5($value['password'])){
-        
+	echo json_encode($fila, true);
 
-	echo json_encode(true, true);
 
-	}
-
-}
 	
 // Liberar resultados
 mysql_free_result($result);
---
+++ Web Services/packetsWS/updateDataCustomer.php
@@ -0,0 +1,39 @@
+
+
+<?php
+
+//$array = json_decode(entity, true);
+
+$value = json_decode(file_get_contents('php://input'), true);
+
+//echo $value['user'];
+//echo md5($value['password']);
+
+
+//consultar en BD
+
+//formato para salida
+
+
+
+// Conectao, seleccionando la base de datos
+$link = mysql_connect('localhost', 'root', '')
+    or die('No se pudo conectar: ' . mysql_error());
+//echo 'Connected successfully';
+mysql_select_db('packets') or die('No se pudo seleccionar la base de datos');
+
+
+// Realizar una consulta MySQL
+$query = "UPDATE customers SET name = '".$value['newName']."', last_name = '".$value['newLastName']."', address = '".$value['newAddress']."', phone = '".$value['newPhoneNumber']."', postal_code = '".$value['newPostalCode']."', city = '".$value['newCity']."', state = '".$value['newState']."', country = '".$value['newCountry']."', email = '".$value['newEmail']."', company_name = '".$value['newCompanyName']."', rfc = '".$value['newRFC']."', position = '".$value['newPosition']."' WHERE username = '".$value['user']."'";
+$result = mysql_query($query) or die('Consulta fallida: ' . mysql_error());
+
+
+	     echo json_encode($result, true);
+
+    
+
+
+// Cerrar la conexión
+mysql_close($link);
+
+?>
